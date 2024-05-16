a230a4e12ea4f1f5a54e3529851ddd9240de3f0f
Shahd El-Badrawy <shahdbadrawy@gmail.com>
2024-05-16 05:16:46 +0300

    proj

---
+++ Components/Department.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace BLAZOR_LIST_BOX.Components
+{
+    public class Department
+    {
+        public string  Id { get; set; }
+        public string DisplayVal { get; set; }
+
+        public List<Employee> employees { get; set; }
+    }
+
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
